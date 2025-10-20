6062c1b233b83225abfed0f88aec4ad1936575c2
Nia-Le1908 <nghiale123528@gmail.com>
2025-10-20 22:27:35 +0700

    Update DBConnection.java

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/quanlydiem?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "123528";
+    private static final String PASSWORD = "Your_password";
 
     public static Connection getConnection() {
         try {
@@ -18,4 +18,5 @@ public class DBConnection {
         }
     }
 }
+
 
